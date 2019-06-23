824b12ac709d22d78705fbefa4a1fddf159d1cde
Chandler Baskins <baskinschandler@gmail.com>
2019-06-22 22:03:26 -0500

    removed env files and setup nodemailer

--- server.js
+++ server.js
@@ -40,22 +40,22 @@ app.post('/apply', (req, res) => {
             // Generate test SMTP service account from ethereal.email
             // Only needed if you don't have a real mail account for testing
             let testAccount = await nodemailer.createTestAccount();
-        
+                    
             // create reusable transporter object using the default SMTP transport
             let transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
                 user: testAccount.user, // generated ethereal user
                 pass: testAccount.pass // generated ethereal password
-            }
+                }
             });
-        
+
             // send mail with defined transport object
             let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            to: "chandlerbaskins@yahoo.com", // list of receivers
+            from: 'chandlerbaskins@yahoo.com', // sender address
+            to: "baskinschandler@gmail.com", // list of receivers
             subject: "Hello ✔", // Subject line
             text: "Hello world?", // plain text body
             html: output // html body
